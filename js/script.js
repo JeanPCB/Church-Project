4c7715e90c12b8005e3aad32f23ccce64d62db0a
JeanPCB <92955454+JeanPCB@users.noreply.github.com>
2021-11-10 19:03:50 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ function scrollFunction() {
     document.body.scrollTop > 20 ||
     document.documentElement.scrollTop > 20
   ) {
-    mybutton.style.display = "block";
+    mybutton.style.display = "inline-block";
   } else {
     mybutton.style.display = "none";
   }
