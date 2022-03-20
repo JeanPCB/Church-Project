a62cbd846fd925d236cc46d7d5933a5323f3b8b6
JeanPCB <92955454+JeanPCB@users.noreply.github.com>
2022-03-20 17:12:23 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ window.onscroll = function () {
 };
 
 function scrollFunction() {
+
   if (
     document.body.scrollTop > 20 ||
     document.documentElement.scrollTop > 20
@@ -20,11 +21,11 @@ function scrollFunction() {
     mybutton.style.display = "none";
     instaButton.style.display = "none";
   }
-}
+};
 // When the user clicks on the button, scroll to the top of the document
 mybutton.addEventListener("click", backToTop);
 
 function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
-}
+};
