35fb50dbae5c2a86dab8f62fa37ad8954c8697ae
JeanPCB <92955454+JeanPCB@users.noreply.github.com>
2022-03-20 22:01:32 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ let instaButton = document.getElementById("insta-button");
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
   scrollFunction();
-};
+}
 
 function scrollFunction() {
 
@@ -21,11 +21,27 @@ function scrollFunction() {
     mybutton.style.display = "none";
     instaButton.style.display = "none";
   }
-};
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
+}
 
+// When the user clicks on the button, scroll to the top of the document
 function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
-};
+}
+
+mybutton.addEventListener("click", backToTop);
+
+// Title Box Padding Toggle
+let titleBox = document.getElementById("title-box-collapsed");
+
+function titleBoxPad() {
+  if (
+    titleBox.id === "title-box-collapsed"
+  ) {
+    titleBox.id = "title-box"
+  } else {
+    titleBox.id = "title-box-collapsed"
+  }
+}
+
+document.getElementById("navbar-toggler-button").addEventListener("click", titleBoxPad);
